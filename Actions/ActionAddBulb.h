e2e964f4d79c9116a417b905a685da5bbefc7b89
AyaOraby <94713813+AyaOraby@users.noreply.github.com>
2021-12-14 00:10:48 +0200

    Add files via upload

---
+++ Actions/ActionAddBulb.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Action.h"
+#include "..\Components\ActionAddBulb.h"
+class ActionAddBulb :public Action
+{
+private:
+	//Parameters for rectangular area to be occupied by the comp
+	int Cx, Cy;	//Center point of the comp
+	int x1, y1, x2, y2;	//Two corners of the rectangluar area
+public:
+	ActionAddBulb(ApplicationManager* pApp);
+	virtual ~ActionAddBulb(void);
+
+	//Execute action 
+	virtual void Execute();
+
+	virtual void Undo();
+	virtual void Redo();
+
+
+};
+
+
+};
+
---
+++ Actions/ActionAddBuzzer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Action.h"
+#include "..\Components\ActionAddBuzzer.h"
+class ActionAddBuzzer :public Action
+{
+private:
+	//Parameters for rectangular area to be occupied by the comp
+	int Cx, Cy;	//Center point of the comp
+	int x1, y1, x2, y2;	//Two corners of the rectangluar area
+public:
+	ActionAddBuzzer(ApplicationManager* pApp);
+	virtual ~ActionAddBuzzer(void);
+
+	//Execute action 
+	virtual void Execute();
+
+	virtual void Undo();
+	virtual void Redo();
+
+
+};
+
+
