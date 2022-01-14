7d707b1b60c2dcc03c5a887f642cc0d0fdc944ae
AyaOraby <94713813+AyaOraby@users.noreply.github.com>
2022-01-14 18:22:29 +0200

    Add files via upload

--- Actions/ActionMove.h
+++ Actions/ActionMove.h
@@ -1,33 +1,22 @@
 #pragma once
-#include "Action.h"
-
-	class ActionMove :
-		public Action
-	{
-		int cx, cy;
-		int x1, y1, x2, y2;
-
-
-	public:
-		ActionMove(ApplicationManager* pApp);
-		virtual ~ActionMove(void);
+#include "..\ApplicationManager.h"
+#include "..\UI\UI.h"
+#include "ActionMove.h"
+class ActionMove :
+	public Action
+{
+	int cx, cy;
+	int x1, y1, x2, y2;
 
 
-		virtual void Execute();
-
-		virtual void Undo();
-		virtual void Redo();
-
-	};
+public:
+	ActionMove(ApplicationManager* pApp);
+	virtual ~ActionMove(void);
 
 
+	virtual void Execute();
 
-
-
-
-
-
-
+	virtual void Undo();
+	virtual void Redo();
 
 };
-
